78cbf7602b0466aeddc53d337200289130e2da2f
Kayla Luebbert <luebbertkayla@gmail.com>
2021-03-19 01:22:50 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,7 +5,7 @@ require 'pry'
 class Song
   
   extend Memorable::ClassMethods 
-  include Paramble::InstanceMethods
+  include Paramable::InstanceMethods
   
   attr_accessor :name
   attr_reader :artist
