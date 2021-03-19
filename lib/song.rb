708f27761454cd88f82a24c3178216cd2cec1616
Kayla Luebbert <luebbertkayla@gmail.com>
2021-03-19 01:33:00 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,7 +5,7 @@ require_relative './concerns/paramable.rb'
 class Song
   
   extend Memorable::ClassMethods 
-  
+  extend Findable::ClassMethods
   include Paramable::InstanceMethods
   include Memorable::InstanceMethods
   
